01ffc3b3c9ab7fade4c129cb1badcf1adc058c20
v.shtyl <v.shtyl@viseven.com>
2024-05-14 12:04:29 +0200

    init project

---
+++ src/api/users-emails/controllers/users-emails.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * users-emails controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::users-emails.users-emails');
---
+++ src/api/users-emails/routes/users-emails.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * users-emails router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::users-emails.users-emails');
